7b19dc7601f24dbee2c888d5b6a8fdbf29b29231
sqb <shenqiangbin@163.com>
2017-11-05 16:07:40 +0800

    commit

---
+++ ChangeVSProjectName/Form1.Designer.cs
@@ -0,0 +1,209 @@
+namespace ChangeVSProjectName
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.textBoxSelectPath = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtKeyWords = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtxResult = new System.Windows.Forms.RichTextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNewText = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(419, 53);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "选择";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // textBoxSelectPath
+            // 
+            this.textBoxSelectPath.Location = new System.Drawing.Point(95, 54);
+            this.textBoxSelectPath.Name = "textBoxSelectPath";
+            this.textBoxSelectPath.Size = new System.Drawing.Size(318, 21);
+            this.textBoxSelectPath.TabIndex = 1;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnExport.Location = new System.Drawing.Point(33, 351);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(465, 39);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "开始替换";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "文件夹";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(467, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "说明 ：将项目Copy出来后，指定要替换的名字进行替换，替换包含文件夹和文件的名字";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 404);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(534, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(110, 17);
+            this.toolStripStatusLabel1.Text = "QQ：1969858717";
+            // 
+            // txtKeyWords
+            // 
+            this.txtKeyWords.Location = new System.Drawing.Point(95, 94);
+            this.txtKeyWords.Name = "txtKeyWords";
+            this.txtKeyWords.Size = new System.Drawing.Size(318, 21);
+            this.txtKeyWords.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "待替换的文字";
+            // 
+            // rtxResult
+            // 
+            this.rtxResult.Location = new System.Drawing.Point(33, 156);
+            this.rtxResult.Name = "rtxResult";
+            this.rtxResult.Size = new System.Drawing.Size(465, 189);
+            this.rtxResult.TabIndex = 7;
+            this.rtxResult.Text = "";
+            this.rtxResult.WordWrap = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(419, 93);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "查找";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "替换后的文字";
+            // 
+            // txtNewText
+            // 
+            this.txtNewText.Location = new System.Drawing.Point(95, 129);
+            this.txtNewText.Name = "txtNewText";
+            this.txtNewText.Size = new System.Drawing.Size(318, 21);
+            this.txtNewText.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 426);
+            this.Controls.Add(this.rtxResult);
+            this.Controls.Add(this.txtNewText);
+            this.Controls.Add(this.txtKeyWords);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.textBoxSelectPath);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSelect);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VS项目中文字替换";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox textBoxSelectPath;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox txtKeyWords;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtxResult;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNewText;
+    }
+}
+
---
+++ ChangeVSProjectName/Form1.cs
@@ -0,0 +1,185 @@
+using ChangeVSProjectName.Lib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ChangeVSProjectName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            var dialogResult = folderBrowserDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                textBoxSelectPath.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxSelectPath.Text))
+            {
+                MessageBox.Show("请选择文件夹");
+                return;
+            }
+            if (string.IsNullOrEmpty(txtKeyWords.Text))
+            {
+                MessageBox.Show("请输入要替换的文件");
+                return;
+            }
+
+            rtxResult.Text = "";
+            string dir = textBoxSelectPath.Text;
+            if (!Directory.Exists(dir))
+            {
+                rtxResult.Text += ("\r\n" + dir + "-不存在");
+                return;
+            }
+            FindTextInDir(new DirectoryInfo(dir), txtKeyWords.Text);
+
+            if (string.IsNullOrEmpty(rtxResult.Text))
+                rtxResult.Text += "\r\n 没有查到";
+            rtxResult.Text += "\r\n 查找完成";
+        }
+
+        private void FindTextInDir(DirectoryInfo dir, string text)
+        {
+            FileInfo[] files = dir.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+            foreach (var item in files)
+            {
+                if (item.Name.Contains(text))
+                    ShowMsg(item.FullName);
+
+                using (StreamReader reader = new StreamReader(item.FullName))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (line.Contains(text))
+                            ShowMsg(line);
+                    }
+                }
+            }
+
+            DirectoryInfo[] children = dir.GetDirectories();
+            foreach (var item in children)
+            {
+                if (item.Name == "bin" || item.Name == "obj")
+                    continue;
+
+                FindTextInDir(item, text);
+            }
+
+            if (dir.Name.Contains(text))
+            {
+                ShowMsg(dir.FullName);
+            }
+        }
+
+        private void ShowMsg(string msg)
+        {
+            rtxResult.AppendText(msg + "\r\n");
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            rtxResult.Text = "";
+            btnExport.Enabled = false;
+            btnExport.Text = "替换中...";
+
+            new Thread(() =>
+            {
+                try
+                {
+
+                    string dir = textBoxSelectPath.Text;
+                    FindTextInDirAndReplace(new DirectoryInfo(dir), txtKeyWords.Text, txtNewText.Text);
+
+                }
+                catch (Exception ex)
+                {
+                    if (rtxResult.InvokeRequired)
+                    {
+                        rtxResult.Invoke(new Action<string>((m) =>
+                        {
+                            rtxResult.Text = ex.Message;
+                        }), "");
+                    }
+                }
+                finally
+                {
+                    if (btnExport.InvokeRequired)
+                    {
+                        btnExport.Invoke(new Action<string>((m) =>
+                        {
+                            btnExport.Enabled = true;
+                            btnExport.Text = "开始导出";
+                        }), "");
+                    }
+                    else
+                    {
+                        btnExport.Enabled = true;
+                        btnExport.Text = "开始导出";
+                    }
+
+                    if (rtxResult.InvokeRequired)
+                    {
+                        rtxResult.Invoke(new Action<string>((m) =>
+                        {
+                            rtxResult.Text += "\r\n完成";
+                        }), "");
+                    }
+                }
+            }).Start();
+        }
+
+        private void FindTextInDirAndReplace(DirectoryInfo dir, string text, string newText)
+        {
+
+            FileInfo[] files = dir.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+            foreach (var item in files)
+            {
+                string content = File.ReadAllText(item.FullName);
+                File.WriteAllText(item.FullName, content.Replace(text, newText));
+
+                if (item.Name.Contains(text))
+                {
+                    var newPath = Path.Combine(Path.GetDirectoryName(item.FullName), item.Name.Replace(text, newText));
+                    File.Move(item.FullName, newPath);
+                }
+
+            }
+
+            DirectoryInfo[] children = dir.GetDirectories();
+            foreach (var item in children)
+            {
+                if (item.Name == "bin" || item.Name == "obj")
+                    continue;
+
+                FindTextInDirAndReplace(item, text, newText);
+            }
+
+            if (dir.Name.Contains(text))
+            {
+                var newPath = Path.Combine(Path.GetDirectoryName(dir.FullName), dir.Name.Replace(text, newText));
+                Directory.Move(dir.FullName, newPath);
+            }
+        }
+    }
+
+
+
+
+}
